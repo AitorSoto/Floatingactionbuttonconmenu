b1c5d180661fdac3cd9898256fd9214b02c06bdb
AitorSoto <aitor.soto@iesdoctorbalmis.com>
2019-10-08 19:45:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/floatingactionbuttonconmenu/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.floatingactionbuttonconmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
